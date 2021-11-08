fcc0fbbad5f035e9751c49a26596484f5caa4194
Majid ARTHAUD <majid.arthaud@eleves.enpc.fr>
2021-11-08 14:05:30 +0100

    ajout de la classe player

---
+++ player.py
@@ -0,0 +1,11 @@
+class Player:
+    id : int
+    money : int
+    position : int
+    
+    def __init__(self, id_num = 0, money = 0, position = 0):
+        self.id = id_num
+        self.money = money
+        self.position = position
+
+        
