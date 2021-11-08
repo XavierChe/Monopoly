93aa1b168b6465f631243f318d9875d85ddbd1a6
Charles-Auguste <charlesaugustegourio@gmail.com>
2021-11-08 14:10:33 +0100

    création classe jeu

---
+++ Monopoly.py
@@ -0,0 +1,14 @@
+from propriete import *
+
+class game:
+    def __init__(self):
+        pass
+
+    def begin_game(self):
+        pass
+
+    def begin_tour(self):
+        pass
+
+    def actu_game(self):
+        pass
